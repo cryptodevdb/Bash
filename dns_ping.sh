45e9391f99f35e7f9ae67130f65afac903dfa0fe
cryptodevdb <crypt0workdb@gmail.com>
2021-01-02 19:56:55 +0100

    Update dns_ping.sh

--- dns_ping.sh
+++ dns_ping.sh
@@ -2,8 +2,7 @@
 echo "A =198.41.0.4 -> Verisign  /n B=199.9.14.201 -> Université Californie du Sud /n C=192.33.4.12 -> Cogent Coms /n D=199.7.91.13 -> Maryland University /n 
 E=192.203.230.10 -> NASA /n F=192.5.5.241 -> Internet System Consortium /n G=192.112.36.4 Defense Information Systems Agency /n  H=198.97.190.53 -> USArmy Research Laboratory /n
 I=192.36.148.17 -> Autonomica J=192.58.128.30 -> Verisign /n K=193.0.14.123 -> RIPE NCC L=199.7.83.42->ICANN /n M=202.12.27.33->WIDE Project"
-
-read "Choisir une lettre correspondant à une IP", p
+read -p "IP de DNS racine à ping : ", p
 
 if p = "A"
 then 
