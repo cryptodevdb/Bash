563456b9c8201fd2c7989c507b8f734ea295e7be
cryptodevdb <crypt0workdb@gmail.com>
2021-01-02 19:50:58 +0100

    Update dns_ping.sh

--- dns_ping.sh
+++ dns_ping.sh
@@ -2,69 +2,70 @@
 echo "A =198.41.0.4 -> Verisign  /n B=199.9.14.201 -> Université Californie du Sud /n C=192.33.4.12 -> Cogent Coms /n D=199.7.91.13 -> Maryland University /n 
 E=192.203.230.10 -> NASA /n F=192.5.5.241 -> Internet System Consortium /n G=192.112.36.4 Defense Information Systems Agency /n  H=198.97.190.53 -> USArmy Research Laboratory /n
 I=192.36.148.17 -> Autonomica J=192.58.128.30 -> Verisign /n K=193.0.14.123 -> RIPE NCC L=199.7.83.42->ICANN /n M=202.12.27.33->WIDE Project"
-read "Choisir une lettre correspondant à une IP", ip 
 
-if ip = "A"
+read "Choisir une lettre correspondant à une IP", p
+
+if p = "A"
 then 
 ping 192.41.0.4
 fi
 
-if ip = "B" 
+if p = "B" 
 then 
 ping 199.9.14.201
 fi
 
-if ip = "C" 
+if p = "C" 
 then 
 ping 192.33.4.12
 fi
 
-if ip = "D" 
+if p = "D" 
 then
 ping 199.7.91.13
 fi
 
-if ip = "E" 
+if p = "E" 
 then 
 ping 192.203.230.10
 fi
 
-if ip = "F" 
+if p = "F" 
 then 
 ping 192.5.5.241
 fi
 
-if  ip = "G" 
+if  p = "G" 
 then 
 ping 192.112.36.4
 fi
 
-if ip = "H" 
+if p = "H" 
 then
 ping 198.97.190.53
 fi
 
-if ip = "I"
+if p = "I"
 then 
 ping 192.36.148.17
 fi
 
-if ip = "J" 
+if p = "J" 
 then 
 ping 192.58.123.30
 fi
 
-if ip = "K" 
+if p = "K" 
 then 
 ping 193.0.14.129
 fi 
 
-if ip = "L" 
+if p = "L" 
 then
 ping 199.7.83.42
 fi
 
-if ip = "M" 
+if p = "M" 
 then
 ping 202.12.27.33
 fi
