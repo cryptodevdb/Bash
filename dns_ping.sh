326d698edeff610d480fb4a43d30dbc16dac4953
cryptodevdb <crypt0workdb@gmail.com>
2021-01-03 17:27:41 +0100

    Update dns_ping.sh

--- dns_ping.sh
+++ dns_ping.sh
@@ -4,31 +4,44 @@ E=192.203.230.10 -> NASA \n F=192.5.5.241 -> Internet System Consortium \n G=192
 I=192.36.148.17 -> Autonomica \n J=192.58.128.30 -> Verisign \n K=193.0.14.123 -> RIPE NCC L=199.7.83.42->ICANN \n M=202.12.27.33->WIDE Project \n \n "
 read -p "IP de DNS racine à ping : " p
 
-if $p="A"
+if [ $p="A" ]
+then 
 ping 192.41.0.4
-elif $p="B"  
+elif [ $p="B" ]  
+then 
 ping 199.9.14.201
-elif $p="C"  
+elif [ $p="C" ]  
+then
 ping 192.33.4.12
-elif $p="D" 
+elif [ $p="D" ] 
+then 
 ping 199.7.91.13
-elif $p="E"  
+elif [ $p="E" ]  
+then 
 ping 192.203.230.10
-elif $p="F" 
+elif [ $p="F" ] 
+then
 ping 192.5.5.241
-elif  $p="G" 
+elif  [ $p="G" ]
+then
 ping 192.112.36.4
-elif $p="H" 
+elif [ $p="H" ]
+then
 ping 198.97.190.53
-elif $p="I"
+elif [ $p="I" ]
+then
 ping 192.36.148.17
-elif $p="J" 
+elif [ $p="J" ] 
+then
 ping 192.58.123.30
-elif $p="K"  
+elif [ $p="K" ] 
+then
 ping 193.0.14.129
-elif $p="L" 
+elif [ $p="L" ] 
+then
 ping 199.7.83.42
-elif $p="M" 
+elif [ $p="M" ] 
+then
 ping 202.12.27.33
 else
 echo "Veuillez réessayer"
